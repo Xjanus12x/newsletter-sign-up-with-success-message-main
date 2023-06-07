78d672e0505f8163f3b8f88506c11935b6775c26
xjanus12x <albertovillacarlos07@gmail.com>
2023-06-07 16:21:56 +0800

    first commit

---
+++ src/JS/main.js
@@ -0,0 +1,29 @@
+const errorMsg = document.querySelector(".input-label > p");
+const emailInput = document.getElementById("email");
+const submitButton = document.querySelector(".button");
+const newsLetter = document.querySelector(".container");
+const userEmail = document.querySelector(".user-email");
+const sucessMsg = document.querySelector(".success");
+const footer = document.querySelector(".footer");
+
+const regex = new RegExp(emailInput.getAttribute("pattern"));
+
+function handleEmailInput() {
+  const isValidEmail = regex.test(emailInput.value);
+  errorMsg.classList.toggle("hidden", isValidEmail);
+  emailInput.dataset.stateError = !isValidEmail;
+}
+
+function handleFormSubmit() {
+  newsLetter.classList.add("hide");
+  userEmail.textContent = emailInput.value;
+  footer.classList.add("hidden");
+  newsLetter.addEventListener("animationend", () => {
+    newsLetter.style.display = "none";
+    sucessMsg.classList.remove("hidden");
+    sucessMsg.classList.add("unhide");
+  });
+}
+
+emailInput.addEventListener("keyup", handleEmailInput);
+submitButton.addEventListener("click", handleFormSubmit);
